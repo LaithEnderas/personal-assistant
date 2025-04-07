1a737cfb7685751c47848698672c10a673c19ad6
LaithEnderas <91076182+LaithEnderas@users.noreply.github.com>
2025-04-08 02:04:26 +0300

    Update record.py

--- models/record.py
+++ models/record.py
@@ -1,1 +1,16 @@
+from .fields import Name, Phone, Email, Address, Birthday
 
+class Record:
+    """
+    - ім’я (Name)
+    - список телефонів (Phone)
+    - email (Email)
+    - адреса (Address)
+    - день народження (Birthday)
+    """
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+        self.email = None
+        self.address = None
+        self.birthday = None
