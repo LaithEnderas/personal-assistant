6248a8b6ad1c79208fb93205c557a9452a666fb3
LaithEnderas <91076182+LaithEnderas@users.noreply.github.com>
2025-04-08 02:03:32 +0300

    Update fields.py

--- models/fields.py
+++ models/fields.py
@@ -1,1 +1,29 @@
+class Field:
+    """Базовий клас для полів контакту."""
+    def __init__(self, value):
+        self.value = value
 
+
+class Name(Field):
+    """Поле для імені контакту."""
+    pass
+
+
+class Phone(Field):
+    """Поле для номера телефону. Має перевіряти, що це 10 цифр."""
+    pass
+
+
+class Email(Field):
+    """Поле для email. Має перевіряти коректність формату."""
+    pass
+
+
+class Address(Field):
+    """Поле для адреси."""
+    pass
+
+
+class Birthday(Field):
+    """Поле для дня народження у форматі DD.MM.YYYY."""
+    pass
