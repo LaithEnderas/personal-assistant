fd0f419076ccf961ae704519923b7a21953c20ec
LaithEnderas <91076182+LaithEnderas@users.noreply.github.com>
2025-04-08 02:04:54 +0300

    Update address_book.py

--- models/address_book.py
+++ models/address_book.py
@@ -1,1 +1,10 @@
+from collections import UserDict
+from .record import Record
+
+class AddressBook(UserDict):
+    """
+    Колекція об'єктів Record. Пошук, додавання, видалення, пошук ДР.
+    """
+    pass
+
 
